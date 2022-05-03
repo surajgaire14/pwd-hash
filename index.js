71422ccf551b73b02fbffb85e6d3705b0b0df6db
The Boring School <theboringschool@Thes-MacBook-Pro.local>
2022-05-03 14:11:22 +0545

    Removed Bugs

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const crypto = require("crypto");
 
 const algorithm = "aes-256-cbc";
 const initVector = crypto.randomBytes(16);
+const Securitykey = crypto.randomBytes(32)
 
-exports.hash = function (password, securityKey) {
+exports.hash = function (password) {
   // deepcode ignore InsecureCipherNoIntegrity
-  const cipher = crypto.createCipheriv(algorithm, securityKey, initVector);
-  let encryptedData = cipher.update(message, "utf8", "hex");
+  const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+  let encryptedData = cipher.update(password, "utf8", "hex");
   encryptedData += cipher.final("hex");
   return encryptedData;
 };
 
-exports.verify = function (hashedPassword, securityKey) {
+exports.verify = function (hashedPassword) {
     // deepcode ignore InsecureCipherNoIntegrity
-    const decipher = crypto.createDecipheriv(algorithm, securityKey, initVector);
+    const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
     let decryptedData = decipher.update(hashedPassword, "hex", "utf8");
     decryptedData += decipher.final("utf8");
     return decryptedData;
