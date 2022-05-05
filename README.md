8bb24b11ee85f5bcc93b2e730375590abc47ddb6
theboringschool <theboringschool.org@gmail.com>
2022-05-05 13:43:17 +0545

    Added Module for 1.0.4

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # pwd-hash
-
-### To Encrypt Password
-use hash(), where parameter is plain text.
+Algorithm: `aes-256-cbc`
+**Note:** Not recommended for password hashing ☢️ (Use `bcrypt` instead, soon we'll have our own 🤗). It's sole purpose is for communication encryption. 🚀
+### To encrypt String
+Use hash(param), where parameter is plain text.
+Returns Encrypted String.
 ```
 const stringHash = require('pwd-hash');
 const hash = stringHash.hash('text');
 ```
 
-### To Decrypt Password
-use verify(), where parameter is hashed string.
+### To decrypt encripted string
+Use decrypt(param), where parameter is hashed string.
+Returns Decrypted String.
 ```
 const stringHash = require('pwd-hash');
-const hash = stringHash.verify('hashed string');
+const hash = stringHash.decrypt('hashed string');
+```
+
+### To verify string
+Use decrypt(param), where parameter is hashed string.
+Returns Decrypted String.
+```
+const stringHash = require('pwd-hash');
+const hash = stringHash.decrypt('hashed string');
 ```
--- index.js
+++ index.js
@@ -4,18 +4,42 @@ const algorithm = "aes-256-cbc";
 const initVector = crypto.randomBytes(16);
 const Securitykey = crypto.randomBytes(32)
 
-exports.hash = function (password) {
+/**
+ * 
+ * @param {*} plainString => Plain String that needs to be hashed
+ * @returns {*} => Hashed String
+ */
+exports.hash = function (plainString) {
   // deepcode ignore InsecureCipherNoIntegrity
   const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
-  let encryptedData = cipher.update(password, "utf8", "hex");
+  let encryptedData = cipher.update(plainString, "utf8", "hex");
   encryptedData += cipher.final("hex");
   return encryptedData;
 };
 
-exports.verify = function (hashedPassword) {
+/**
+ * 
+ * @param {*} hashedString => Hashed String that needs to be decrypted
+ * @returns {*} => Decrypted String
+ */
+exports.decrypt = function (hashedString) {
     // deepcode ignore InsecureCipherNoIntegrity
     const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
-    let decryptedData = decipher.update(hashedPassword, "hex", "utf8");
+    let decryptedData = decipher.update(hashedString, "hex", "utf8");
     decryptedData += decipher.final("utf8");
     return decryptedData;
+}
+
+/**
+ * 
+ * @param {*} string 
+ * @param {*} hashedString 
+ * @returns {*} => Boolean ? True if the string is equal to the hashed string : False if not
+ */
+exports.verify = function (string, hashedString) {
+    // deepcode ignore InsecureCipherNoIntegrity
+    const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+    let decryptedData = decipher.update(hashedString, "hex", "utf8");
+    decryptedData += decipher.final("utf8");
+    return string === decryptedData;
 }
